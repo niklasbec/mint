6fd67ae67374cae4d9db6ee6d056ba3ec43f62cc
niklasbec <niklasbeckerr@gmail.com>
2020-05-16 22:14:10 +0200

    2nd

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
-import { NavLink } from "react-router-dom";
 import hoverEffect from "hover-effect";
 import LetterDrop from "./Letterdrop";
 
@@ -20,39 +19,73 @@ function App() {
     });
   });
 
+  const [viewToggle, setViewToggle] = useState(false);
+  const [signUpToggle, setSignUpToggle] = useState(false);
+  const x = event => {
+    event.preventDefault();
+    setViewToggle(!viewToggle);
+  };
+
+  // const y = (event) => {
+  //   event.preventDefault()
+  //   setSignUpToggle(!signUpToggle)
+  // }
+
   return (
     <div className="App">
+      {/*
+    {signUpToggle ? <div className="sign-up">
+    <div className="signup">
+    
+    </div>
+    </div> : null}
+    */}
       <div className="header">
-      <a href="/">
-        <h2>
-          mint<span style={{ color: "black" }}>.</span>
-        </h2>
-      </a>
-      <div className="sign-login">
-        <NavLink exact to="/login" activeStyle={{ color: "#3fc988" }}>
-          Login
-        </NavLink>
-        <p className="spacer"> | </p>
-        <NavLink exact to="/register" activeStyle={{ color: "#3fc988" }}>
-          Sign Up
-        </NavLink>
-      </div>
-      </div>
-      <div className="ticket">
-        <div className="overlay"></div>
-        <div className="flight-info">
-          <h3 className="flight-date">DEC 30 2020 12:30PM</h3>
-          <div className="flight-locations">
-            <h1>JFK</h1>
-            <img src="https://cdn-images-1.medium.com/max/800/1*QFU_XYAKOGJ9nunfqvem1w.png" />
-            <h1>LHR</h1>
-          </div>
+        <a href="/">
+          <h2>
+            mint<span style={{ color: "black" }}>.</span>
+          </h2>
+        </a>
+        <div className="sign-login">
+          <a onClick={() => {alert("This is just a dummy you can't actually sign up to anything")}}>Sign Up</a>
+          <p className="spacer"> | </p>
+          <a onClick={() => {alert("This is just a dummy you can't actually sign up to anything")}}>Unregister</a>
         </div>
       </div>
       <div className="background-deco" />
       <div className="background-deco2" />
-      <div className="triangle-up" />
-      <LetterDrop />
+      {viewToggle ? (
+        <div className="blender">
+          <p className="explanation">
+            Sign Up in the top right corner and get the best flight deals
+            straight to your inbox. We will send you awesome flight offers once
+            a week, and if you don't want to see our offers anymore, simply
+            unsubscribe straight on our website.
+          </p>
+          <a onClick={x}>
+            <div className="see-more">Back</div>
+          </a>
+        </div>
+      ) : (
+        <div className="blender">
+          <div className="ticket">
+            <div className="overlay"></div>
+            <div className="flight-info">
+              <h3 className="flight-date">DEC 30 2020 12:30PM</h3>
+              <div className="flight-locations">
+                <h1>JFK</h1>
+                <img src="https://cdn-images-1.medium.com/max/800/1*QFU_XYAKOGJ9nunfqvem1w.png" />
+                <h1>LHR</h1>
+              </div>
+            </div>
+          </div>
+          <div className="triangle-up" />
+          <LetterDrop toggle={viewToggle} />
+          <a onClick={x}>
+            <div className="see-more">How it works</div>
+          </a>
+        </div>
+      )}
     </div>
   );
 }
--- src/Letterdrop.js
+++ src/Letterdrop.js
@@ -1,74 +1,77 @@
 import React, { useEffect } from "react";
 
-function LetterDrop() {
-  
-    useEffect(() => {
+function LetterDrop(props) {
+  useEffect(() => {
+    let words = document.getElementsByClassName("word");
+    let wordArray = [];
+    let currentWord = 0;
 
-let words = document.getElementsByClassName('word');
-let wordArray = [];
-let currentWord = 0;
+    words[currentWord].style.opacity = 1;
+    for (let i = 0; i < words.length; i++) {
+      splitLetters(words[i]);
+    }
 
-words[currentWord].style.opacity = 1;
-for (let i = 0; i < words.length; i++) {
-  splitLetters(words[i]);
-}
+    function changeWord() {
+      let cw = wordArray[currentWord];
+      let nw =
+        currentWord == words.length - 1
+          ? wordArray[0]
+          : wordArray[currentWord + 1];
+      for (let i = 0; i < cw.length; i++) {
+        animateLetterOut(cw, i);
+      }
 
-function changeWord() {
-  let cw = wordArray[currentWord];
-  let nw = currentWord == words.length-1 ? wordArray[0] : wordArray[currentWord+1];
-  for (let i = 0; i < cw.length; i++) {
-    animateLetterOut(cw, i);
-  }
-  
-  for (let i = 0; i < nw.length; i++) {
-    nw[i].className = 'letter behind';
-    nw[0].parentElement.style.opacity = 1;
-    animateLetterIn(nw, i);
-  }
-  
-  currentWord = (currentWord == wordArray.length-1) ? 0 : currentWord+1;
-}
+      if (nw) {
+        for (let i = 0; i < nw.length; i++) {
+          nw[i].className = "letter behind";
+          nw[0].parentElement.style.opacity = 1;
+          animateLetterIn(nw, i);
+        }
+      }
 
-function animateLetterOut(cw, i) {
-  setTimeout(function() {
-		cw[i].className = 'letter out';
-  }, i*80);
-}
+      currentWord = currentWord == wordArray.length - 1 ? 0 : currentWord + 1;
+    }
 
-function animateLetterIn(nw, i) {
-  setTimeout(function() {
-		nw[i].className = 'letter in';
-  }, 340+(i*80));
-}
+    function animateLetterOut(cw, i) {
+      setTimeout(function() {
+        cw[i].className = "letter out";
+      }, i * 80);
+    }
 
-function splitLetters(word) {
-  let content = word.innerHTML;
-  word.innerHTML = '';
-  let letters = [];
-  for (let i = 0; i < content.length; i++) {
-    let letter = document.createElement('span');
-    letter.className = 'letter';
-    letter.innerHTML = content.charAt(i);
-    word.appendChild(letter);
-    letters.push(letter);
-  }
-  
-  wordArray.push(letters);
-}
+    function animateLetterIn(nw, i) {
+      setTimeout(function() {
+        nw[i].className = "letter in";
+      }, 340 + i * 80);
+    }
 
-changeWord();
-setInterval(changeWord, 4000);
-})
+    function splitLetters(word) {
+      let content = word.innerHTML;
+      word.innerHTML = "";
+      let letters = [];
+      for (let i = 0; i < content.length; i++) {
+        let letter = document.createElement("span");
+        letter.className = "letter";
+        letter.innerHTML = content.charAt(i);
+        word.appendChild(letter);
+        letters.push(letter);
+      }
+
+      wordArray.push(letters);
+    }
+
+    changeWord();
+    setInterval(changeWord, 4000);
+  });
 
   return (
     <div className="letter-drop">
-    <div className="text">
-    <p className="mint">Mint is&nbsp;</p>
-    <span className="word midnight">awesome.</span>
-    <span className="word belize">reliable.</span>
-    <span className="word green">affordable.</span>
-    <span className="word wisteria">smart.</span>
-    </div>
+      <div className="text">
+        <p className="mint">Mint is&nbsp;</p>
+        <span className="word midnight">awesome.</span>
+        <span className="word belize">reliable.</span>
+        <span className="word green">affordable.</span>
+        <span className="word wisteria">smart.</span>
+      </div>
     </div>
   );
 }
